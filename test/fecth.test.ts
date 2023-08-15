27f16cfeab87b2f076b44986c7659f60bef28cb2
阳九 <871921282@qq.com>
2023-08-15 16:12:14 +0800

    编写外层Fetch模块测试

---
+++ test/fecth.test.ts
@@ -0,0 +1,59 @@
+/*
+ * @Author: Luzy
+ * @Date: 2023-08-15 14:18:39
+ * @LastEditors: Luzy
+ * @LastEditTime: 2023-08-15 16:07:56
+ * @Description: 
+ */
+
+import Fetch from "../src/Fetch";
+
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+
+// 模拟网络请求数据
+const url = "http://localhost:8848/test"
+const headers = { "Content-Type": "application/json" }
+
+// 外层请求模块测试
+describe('Fecth module', () => {
+
+
+    const testRes = (res: any) => {
+        expect(res.status).toBe(200)
+        expect(res.message).not.toBeNull()
+    }
+
+    test('get', async () => {
+        const res = await Fetch.get(url, { headers })
+        testRes(res)
+    });
+
+    test('post', async () => {
+        const res = await Fetch.post(url, { headers })
+        testRes(res)
+    });
+
+    test('delete', async () => {
+        const res = await Fetch.delete(url, { headers })
+        testRes(res)
+    });
+
+    test('put', async () => {
+        const res = await Fetch.put(url, { headers })
+        testRes(res)
+    });
+
+    test('patch', async () => {
+        const res = await Fetch.patch(url, { headers })
+        testRes(res)
+    });
+
+    test('head', async () => {
+        const res = await Fetch.head(url, { headers })
+        testRes(res)
+    });
+});
